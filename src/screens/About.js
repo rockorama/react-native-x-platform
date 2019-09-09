6e45bcaf7d2343570ddf51b43806c9826cf9ef48
Diogo Perillo <diogo.perillo@gmail.com>
2019-09-09 00:05:06 -0600

    move dimensions to about

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,14 +1,20 @@
 // @flow
 
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React, { useContext } from 'react'
+import { StyleSheet, Text, View, Platform, Button } from 'react-native'
 import { type NavigationScreenProps } from 'react-navigation'
+import Context from '../context'
 
 const About = (props: NavigationScreenProps) => {
+  const { screenSize } = useContext(Context)
   return (
     <View style={styles.container}>
-      <Text>About</Text>
-      <Button title="Back Home" onPress={() => props.navigation.goBack()} />
+      <Text style={styles.title}>About!</Text>
+      <Text> Platform: {Platform.OS}</Text>
+      <Text>
+        Screen Size: {screenSize.width} x {screenSize.height}
+      </Text>
+      <Button title="Back" onPress={() => props.navigation.navigate('/')} />
     </View>
   )
 }
@@ -16,6 +22,9 @@ const About = (props: NavigationScreenProps) => {
 export default About
 
 const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'Roboto-Bold',
+  },
   container: {
     flex: 1,
     // backgroundColor: '#fff',
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,20 +1,14 @@
 // @flow
 
-import React, { useContext } from 'react'
-import { StyleSheet, Text, View, Platform, Button } from 'react-native'
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import { type NavigationScreenProps } from 'react-navigation'
-import Context from '../context'
 
 const Home = (props: NavigationScreenProps) => {
-  const { screenSize } = useContext(Context)
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to React Native X-Platform app!</Text>
-      <Text>Start editing `Root.js`</Text>
-      <Text> Platform: {Platform.OS}</Text>
-      <Text>
-        Screen Size: {screenSize.width} x {screenSize.height}
-      </Text>
+      <Text>Start editing `src/screens/Home.js`</Text>
       <Button
         title="About"
         onPress={() => props.navigation.navigate('/about')}
